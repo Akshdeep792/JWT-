f66d6d905c414d833e8d931d4bac594858048372
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-01-01 22:38:02 -0500

    Complete

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,25 @@
-
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+const customAPIError = require('../errors/custom-error')
 
 const loggin = async (req,res) => {
-    res.send('You are logged in....!');
+    const {username, password} = req.body;
+
+    if(!username || !password){
+        throw new customAPIError('Please provide email and password', 400);
+    }
+    const id = new Date().getDate();
+    const token = jwt.sign({id,username}, process.env.JWT_SECRET, {expiresIn : '30d'})
+
+    res.status(200).json({msg: 'user created', token})
 }
 
 
 const dashboard = async (req,res) => {
+    
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(201).json({msg : `Hello Akshdeep Singh`, secret : `Your Lucky number is ${luckyNumber}`})
+    res.status(201).json({msg : `Hello ${req.user.username}`, secret : `Here is your authorized data and, Your Lucky number is ${luckyNumber}`})
+
 }
 
 module.exports = {
--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,9 @@ const express = require("express")
 const router = express.Router();
 
 const {loggin, dashboard} = require('../controllers/main');
+const authMiddleware = require('../middleware/auth')
 
-router.route('/dashboard').get(dashboard);
+router.route('/dashboard').get(authMiddleware,dashboard);
 router.route('/login').post(loggin)
 
 module.exports = router
